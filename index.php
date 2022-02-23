b18433932a6bc96fefeed6c8a0cd5dde98c2f8e1
Rohit Rawat <100186307+rohitrawatcodes@users.noreply.github.com>
2022-02-23 14:23:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,12 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
+    <?php
+    
+    $a = "Hello Welcome";
+    echo $a;
+    
+    ?>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
